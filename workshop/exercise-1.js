47bd308e59b98ee24f26568e098bc09d347aa94e
Ning An <ning.an@outlook.com>
2020-06-23 13:43:31 -0400

    Exercise1-solution

--- workshop/exercise-1.js
+++ workshop/exercise-1.js
@@ -12,3 +12,19 @@ let mostPopularFood = [
   'parmesan chicken',
   undefined,
 ];
+
+const fixList = (arr, first, last) => {
+  const newArr = [...arr];
+  newArr.unshift(first);
+  newArr.pop();
+  newArr.push(last);
+  return newArr;
+}
+
+// console.log(fixList(mostPopularFood, 'bean burritos', 'buffalo-flavored cauliflower'))
+
+const printList = (arr) => {
+  arr.forEach( (elem, i) => console.log(`${i+1}. ${elem}`) );
+}
+
+printList(fixList(mostPopularFood, 'bean burritos', 'buffalo-flavored cauliflower'));
