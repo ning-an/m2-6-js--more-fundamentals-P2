75e0b2f440e09c05e0f14b6bb302c8bcc892f02c
Ning An <ning.an@outlook.com>
2020-06-23 13:50:35 -0400

    Exercise2-solution

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -3,3 +3,11 @@
 
 const lateComers = ['Felicia', 'Charles', 'Terry'];
 const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
+
+const printGuestOrder = (early, late) => {
+    const final = [...early];
+    late.forEach( elem => final.push(elem) );
+    final.forEach( elem => console.log(elem) );
+}
+
+printGuestOrder(earlyBirds, lateComers);
