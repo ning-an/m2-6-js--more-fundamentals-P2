36adffcfd1c6ecfa168e0baf6ad493a0d48f698a
Ning An <ning.an@outlook.com>
2020-06-23 11:24:11 -0400

    Tic-tac-toe solution

--- __lecture/tic-tac-toe/app.js
+++ __lecture/tic-tac-toe/app.js
@@ -8,7 +8,7 @@ const endMessageDiv = document.querySelector('.js-end-message');
 const restartBtn = document.querySelector('.restart');
 
 // this array tracks the state of the game board.
-const game = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+let game = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 // Game Settings
 const PLAYER_1_ICON = 'X';
@@ -22,10 +22,23 @@ player1.classList.add('active');
 const handleRestart = () => {
   // reload the page
   // feels like cheating but it WILL reset the game
+  game = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  currentPlayer = '1';
+  player1.classList.add('active');
+  player2.classList.remove('active');
+  let cells = document.querySelectorAll('.cell');
+  cells.forEach(elem => elem.innerText = '');
+  endMessageDiv.innerText = '';
+  board.addEventListener('click', handleClick);
+
+  //window.location.reload();
 };
+restartBtn.addEventListener('click', handleRestart);
+
 
 const toggleRestartBtn = () => {
   // enable the restart btn
+  restartBtn.disabled = restartBtn.disabled ? false : true;
 };
 
 const win = () => {
@@ -36,10 +49,30 @@ const win = () => {
 
 const verifyWin = () => {
   // Use the game array to determine the winner.
+  if ((game[0] == game[1] && game[1] == game[2]) || 
+      (game[3] == game[4] && game[4] == game[5]) ||
+      (game[6] == game[7] && game[7] == game[8]) ||
+      (game[0] == game[3] && game[3] == game[6]) ||
+      (game[1] == game[4] && game[4] == game[7]) ||
+      (game[2] == game[5] && game[5] == game[8]) ||
+      (game[2] == game[4] && game[4] == game[6]) ||
+      (game[0] == game[4] && game[4] == game[8])) {
+        endMessageDiv.innerText = `Player ${currentPlayer} Wins!`;
+        toggleRestartBtn();
+        board.removeEventListener('click', handleClick);
+      } else if (game.every( elem => elem === 'X' || elem === 'O')) {
+        endMessageDiv.innerText = 'Draws!'
+        toggleRestartBtn();
+        board.removeEventListener('click', handleClick);
+
+      }
 };
 
 const togglePlayer = () => {
   // use .active to show active player visually...
+  currentPlayer = currentPlayer === '1' ? '2' : '1';
+  player1.classList.toggle('active');
+  player2.classList.toggle('active');
 };
 
 const handleClick = (event) => {
@@ -52,6 +85,9 @@ const handleClick = (event) => {
     currentCellDiv.innerText = icon;
 
     // so much missing here...
+    game[cellId] = icon;
+    verifyWin();
+    togglePlayer();
   }
 };
 
