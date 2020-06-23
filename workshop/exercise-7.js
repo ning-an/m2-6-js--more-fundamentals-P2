23b9969f27823d6824f443481cdbbb7bd06f5ef7
Ning An <ning.an@outlook.com>
2020-06-23 17:04:08 -0400

    Exercise7-solution

--- workshop/exercise-7.js
+++ workshop/exercise-7.js
@@ -9,8 +9,15 @@ let foodPairings = {
 };
 
 // createRecommendations function
-
+const createRecommendations = obj => {
+  const keys = Object.keys(obj);
+  return keys.map( key => {
+    return `With ${key}, it is best to have ${obj[key]}`;
+  })
+}
 // printRecommendations function
-
+const printRecommendations = arr => {
+  arr.forEach( elem => console.log(` - ${elem}`));
+}
 // function call (done)
 printRecommendations(createRecommendations(foodPairings));
